db016e4c43c8ea8c90e3b8aecf30c474e2026e1e
Gleb <podiumfffuuu@gmail.com>
2023-05-22 15:22:30 +0200

    feat: add post to db and create trpc procedure to get it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,12 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 
+import { api } from "~/utils/api";
+
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -12,9 +16,14 @@ const Home: NextPage = () => {
         <meta name="description" content="Modern Web Tutorial" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b text-black">
         <div>{!user.isSignedIn ? <SignInButton /> : <SignOutButton />}</div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
