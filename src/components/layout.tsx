a89877e901786a62f2d24907aa36eee1a7a10fc5
Gleb <podiumfffuuu@gmail.com>
2023-05-30 13:47:21 +0200

    feat: h

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from "react";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-[100dvh] justify-center ">
+    <main className="flex h-[100svh] justify-center md:h-screen ">
       <div className="grid w-full grid-rows-[auto_1fr] overflow-hidden md:max-w-2xl">
         {props.children}
       </div>
